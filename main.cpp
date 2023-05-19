88358e1f3ccf295190bec792c35c134715bfc278
Sergio Berteli Batista Filho <sergio@univali.br>
2023-05-19 12:59:58 -0300

    Novo menu
    Criação da classe menu e da classe locadora

--- main.cpp
+++ main.cpp
@@ -1,9 +1,71 @@
 #include <iostream>
+#include "list.h"
 #include "pessoa.h"
 using namespace std;
 
+class Filme;
+
+
+class Cliente;
+
+
+class Funcionario;
+
+
+class Estoque
+{
+private:
+    ListaDE<Filme> filmes;
+public:
+};
+
+class Locadora
+{
+private:
+    string nome;
+    string CNPJ;
+    ListaDE<Cliente> clientes;
+    ListaDE<Funcionario> funcionarios;
+    Estoque estoque;
+public:
+    Locadora(string nome, string CNPJ);
+};
+
+Locadora::Locadora(string nome, string CNPJ)
+{
+    this->CNPJ = CNPJ;
+    this->nome = nome;
+}
+
+class Menu
+{
+private:
+public:
+    void main();
+    ~Menu();
+};
+
+Menu::~Menu()
+{
+    cout<<"Obrigado por usar a nossa locadora!";
+}
+
+void Menu::main() {
+    Locadora * loc;
+    string nome, CNPJ;
+    cout<<"Insira o nome da Locadora: ";
+    cin>>nome;
+    cout<<"Insira o cnpj dela: ";
+    cin>>CNPJ;
+    loc = new Locadora(nome, CNPJ);
+
+    cout << "Bem-vindo a locadora " <<nome<<"!"<<endl<< endl;
+}
+
+
 int main(int argc, char const *argv[])
 {
-    Hello();
+    Menu m;
+    m.main();
     return 0;
 }
